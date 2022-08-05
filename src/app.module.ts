051c9ea7db04a68639550c8f0c55e883f325ed75
kevit-khushali-dhami <Khushali.Dhami@kevit.io>
2022-08-05 19:02:53 +0530

    Modi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import AttendeeModule from './attendees/attendee.module';
+import EventModule from './events/event.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGODB_URL),
+    EventModule,
+    AttendeeModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
